2071afdb03a68d76c81dd132735dcad965ce76bf
drewH7770 <125577538+drewH7770@users.noreply.github.com>
2024-03-24 15:04:36 -0400

    BaseLine

---
+++ screens/home.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const APIKey = "5c415b93-66df-4422-a506-10b9a006532e"
+const APITeams = "http://api.balldontlie.io/v1/teams"
+
+const Home = () => {
+
+    useEffect(()=>{
+        fetch({})
+    },[])
+
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+})
