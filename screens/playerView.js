adf316cc55e4691868062a3fc43e704d769ef8f5
Hazen8988 <patrickwildgen@gmail.com>
2024-04-09 23:56:00 -0400

    Patrick's Last commit

--- screens/playerView.js
+++ screens/playerView.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import axios from 'axios'; 
-
+import { Table, Row, Rows } from 'react-native-table-component';
 
 const PlayerView = ({ route }) => {
     const { player } = route.params;// extracting player ID from route parameter
     const [ playerDetails, setPlayerDetails ] = useState(null);
-    console.log("Player obj from Coming from gameView: ", player);
-    console.log("team id: ", player.team_id)
+    const [ seasonAverage, setSeasonAverage ] = useState(null);
+
+    // console.log("Player obj from Coming from gameView: ", player);
+    // console.log("player id: ", player.id)
 
     useEffect(() => {
         // Fetch player details when component mounts
@@ -21,14 +23,23 @@ const PlayerView = ({ route }) => {
                 , {headers})
 
                 const teamData = playerResponse.data;
-                console.log("team data:", teamData);
+                // console.log("team data:", teamData);
 
-                const playerDetails = teamData.data.filter(details => details.last_name === player.last_name 
-                    && details.first_name === player.first_name);
-                console.log("Player Details: " , playerDetails)
+                const playerDetails = teamData.data.find(details => details.last_name === player.last_name && details.first_name === player.first_name);
+                // console.log("Player Details from filter: ", playerDetails)
                 // const playerData = playerResponse.data.data; // Assuming the first result is the desired player
                 // console.log(playerData)
-                setPlayerDetails(teamData);
+                setPlayerDetails(playerDetails);
+                // console.log("Player details id: ", playerDetails.id)
+
+                const seasonAverageResponse = await axios.get(`https://api.balldontlie.io/v1/season_averages?season=2023&player_ids[]=${player.id}`
+                , { headers });
+                const seasonAverageData = seasonAverageResponse.data;
+                // console.log("Season Average of Player: ",seasonAverageData);
+                setSeasonAverage(seasonAverageData);
+
+
+
             } catch (error) {
                 console.error('Error fetching player details: ', error);
             }
@@ -38,46 +49,136 @@ const PlayerView = ({ route }) => {
         //clear state when component unmounts
         return () => {
             setPlayerDetails(null);
+            setSeasonAverage(null);
           };
 
     }, [player]);
-    return (
-        <View style={styles.container}>
-          {/* {playerDetails ? (
-            <View>
-              <Text style={styles.playerName}>
-                {playerDetails.first_name} {playerDetails.last_name}
-              </Text>
-              <Text>Position: {playerDetails.position}</Text>
-              <Text>Height: {playerDetails.height}</Text>
-              <Text>Weight: {playerDetails.weight} lbs</Text>
-              <Text>Jersey Number: {playerDetails.jersey_number}</Text>
-              <Text>College: {playerDetails.college}</Text>
-              <Text>Country: {playerDetails.country}</Text>
-              <Text>Draft Year: {playerDetails.draft_year}</Text>
-              <Text>Draft Round: {playerDetails.draft_round}</Text>
-              <Text>Draft Number: {playerDetails.draft_number}</Text>
-              <Text>Team: {playerDetails.team.full_name}</Text>
-            </View>
-          ) : (
-            <Text>Loading player details...</Text>
-          )} */}
-        </View>
-      );
+    const renderPlayerDetailsTable = () => {
+        if (!playerDetails) return null;
+        const tableData = [
+            ['Position', playerDetails.position],
+            ['Height', playerDetails.height],
+            ['Weight', `${playerDetails.weight} lbs`],
+            ['Jersey Number', playerDetails.jersey_number],
+            ['College', playerDetails.college],
+            ['Country', playerDetails.country],
+            ['Draft Year', playerDetails.draft_year],
+            ['Draft Round', playerDetails.draft_round],
+            ['Draft Number', playerDetails.draft_number],
+            ['Team', playerDetails.team.full_name]
+        ];
+        return (
+            <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+                <Rows data={tableData} textStyle={styles.tableText} />
+            </Table>
+        );
     };
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        padding: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      playerName: {
+
+    const renderSeasonAveragesCards = () => {
+        if (!seasonAverage) return null;
+        const { data } = seasonAverage;
+        return data.map(item => (
+            <View key={item.id}>
+                {/* <Text style={styles.card}>Season: {item.season}</Text> */}
+                <View style={styles.card}>
+                    <Text>Games Played: {item.games_played}</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text>Minutes Per Game: {item.min}</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text>Points Per Game: {item.pts.toFixed(2)}</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text>Assists Per Game: {item.ast.toFixed(2)}</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text>Rebounds Per Game: {item.reb.toFixed(2)}</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text>Blocks Per Game: {item.blk.toFixed(2)}</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text>Turnovers Per Game: {item.turnover.toFixed(2)}</Text>
+                </View>
+            </View>
+        ));
+    };
+
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            {playerDetails ? (
+                <View>
+                    <Text style={styles.playerName}>{playerDetails.first_name} {playerDetails.last_name}</Text>
+                    {renderPlayerDetailsTable()}
+                </View>
+            ) : (
+                <Text>Loading player details...</Text>
+            )}
+            {seasonAverage ? (
+                <View>
+                    <Text style={styles.sectionTitle}>Season Averages for 2023</Text>
+                    <ScrollView horizontal={true}>
+                        <View style={styles.cardsContainer}>
+                            {renderSeasonAveragesCards()}
+                        </View>
+                    </ScrollView>
+                </View>
+            ) : (
+                <Text>Loading season averages...</Text>
+            )}
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    statContainer: {
+        marginBottom: 5,
+    },
+    container: {
+        flexGrow: 1,
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+    },
+    playerName: {
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 10,
-      },
-    });
-    
-    export default PlayerView;
+    },
+    sectionTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    tableText: {
+        margin: 6,
+    },
+    card: {
+        width: 300,
+        backgroundColor: '#ffffff',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 15,
+        marginBottom: 20,
+        marginRight: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    cardTitle: {
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    cardsContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+});
+
+export default PlayerView;
